a7e2bb03592b0ef4f85368246cffafcc481edd1f
Abbas-Lokhandwala <abbasanishlok@gmail.com>
2025-08-08 16:09:00 -0600

    heha

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,74 +1,55 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
-export const Navigation = (props) => {
+export const Navigation = () => {
   return (
-    <nav id="menu" className="navbar navbar-default navbar-fixed-top">
-      <div className="container">
-        <div className="navbar-header">
-          <button
-            type="button"
-            className="navbar-toggle collapsed"
-            data-toggle="collapse"
-            data-target="#bs-example-navbar-collapse-1"
-          >
-            {" "}
-            <span className="sr-only">Toggle navigation</span>{" "}
-            <span className="icon-bar"></span>{" "}
-            <span className="icon-bar"></span>{" "}
-            <span className="icon-bar"></span>{" "}
-          </button>
-          <a
-            className="navbar-brand page-scroll"
-            href="#page-top"
-            style={{ display: "flex", alignItems: "center", gap: "12px" }}
-          >
-            <img
-              src={`${process.env.PUBLIC_URL}/img/logo.png`}
-              alt="SentiStock Logo"
-              style={{
-                height: "48px",
-                width: "48px",
-                objectFit: "contain",
-              }}
-            />
-            <span style={{ fontWeight: "900", fontSize: "26px", letterSpacing: "1px", color: "#333" }}>
-              SENTISTOCK
-            </span>
-          </a>
-        </div>
-
-        <div
-          className="collapse navbar-collapse"
-          id="bs-example-navbar-collapse-1"
+    <nav
+      id="menu"
+      className="navbar"
+      style={{
+        backgroundColor: "#0a192f",
+        padding: "15px 30px",
+        borderBottom: "1px solid rgba(255, 255, 255, 0.05)",
+        boxShadow: "0 0 12px rgba(0, 255, 255, 0.1)"
+      }}
+    >
+      <div
+        className="container"
+        style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}
+      >
+        <a
+          className="navbar-brand page-scroll"
+          href="#page-top"
+          style={{ display: "flex", alignItems: "center", gap: "12px" }}
         >
-          <ul className="nav navbar-nav navbar-right">
-            <li>
-              <a href="#about" className="page-scroll">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#testimonials" className="page-scroll">
-                Testimonials
-              </a>
-            </li>
-            <li>
-              <a href="#team" className="page-scroll">
-                Team
-              </a>
-            </li>
-            <li>
-              <a href="#contact" className="page-scroll">
-                Contact
-              </a>
-            </li>
-            <li>
-              <Link to="/auth">Register/Sign In</Link>
-            </li>
-          </ul>
-        </div>
+          <img
+            src="img/logo.png"
+            alt="SentiStock Logo"
+            style={{
+              height: "48px",
+              width: "48px",
+              objectFit: "contain"
+            }}
+          />
+          <span
+            style={{
+              fontWeight: "900",
+              fontSize: "26px",
+              letterSpacing: "1px",
+              color: "#5ca9fb"
+            }}
+          >
+            SENTISTOCK
+          </span>
+        </a>
+
+        <ul className="nav navbar-nav navbar-right" style={{ display: "flex", gap: "20px" }}>
+          <li><a href="#about" className="page-scroll" style={{ color: "#fff" }}>About</a></li>
+          <li><a href="#testimonials" className="page-scroll" style={{ color: "#fff" }}>Testimonials</a></li>
+          <li><a href="#team" className="page-scroll" style={{ color: "#fff" }}>Team</a></li>
+          <li><a href="#contact" className="page-scroll" style={{ color: "#fff" }}>Contact</a></li>
+          <li><Link to="/auth" style={{ color: "#5ca9fb", fontWeight: "bold" }}>Register/Sign In</Link></li>
+        </ul>
       </div>
     </nav>
   );
