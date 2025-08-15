f45f4e3b6f79fc146943a72153043035610c4248
Abbas-Lokhandwala <abbasanishlok@gmail.com>
2025-08-14 18:52:22 -0600

    please dont crash 2.0

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -13,7 +13,6 @@ export const Navigation = () => {
         <ul className="nav navbar-nav navbar-right nav-links">
           <li><a href="#about" className="page-scroll">About</a></li>
           <li><a href="#testimonials" className="page-scroll">Testimonials</a></li>
-          <li><a href="#team" className="page-scroll">Team</a></li>
           <li><a href="#contact" className="page-scroll">Contact</a></li>
           <li><Link to="/auth" className="auth-link">Register/Sign In</Link></li>
         </ul>
